77b1499376b9a3f9e20ca12c5ec3809d9b97b05e
xMTunesx <48794668+xMTunesx@users.noreply.github.com>
2025-02-26 10:06:54 +0100

    init

---
+++ .mocharc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  recursive: true,
+  reporter: 'spec',
+  timeout: 5000,
+  require: 'test/setup.js',
+  spec: 'test/**/*.test.js'
+};
---
+++ test/hello.test.js
@@ -0,0 +1,6 @@
+describe('Hello World Test', () => {
+    it('should return true', () => {
+        const result = true;
+        assert.strictEqual(result, true);
+    });
+});
